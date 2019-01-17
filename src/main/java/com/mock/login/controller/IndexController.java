d963dd20e0e405a868bf3e3bfe667fbcf68a9787
tijun <15761635397@163.com>
2019-01-18 02:28:50 +0800

    Initial commit

---
+++ src/main/java/com/mock/login/controller/IndexController.java
@@ -0,0 +1,14 @@
+package com.mock.login.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+public class IndexController {
+
+
+
+    @RequestMapping("index")
+    public Object index(){
+        return new ModelAndView("index");
+    }
+}
