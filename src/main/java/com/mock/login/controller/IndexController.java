83662c7c207b36cf42e3ace64c48b8e8708a18a8
origin <15761635397@163.com>
2019-01-18 18:11:09 +0800

    mock login

--- src/main/java/com/mock/login/controller/IndexController.java
+++ src/main/java/com/mock/login/controller/IndexController.java
@@ -1,10 +1,14 @@
 package com.mock.login.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -12,22 +16,40 @@ import java.io.IOException;
 @Controller
 public class IndexController {
 
+    public final static Logger logger = LoggerFactory.getLogger(IndexController.class);
 
-
-    @RequestMapping("/index")
-    public Object index(){
+    @GetMapping("/index")
+    public Object index(HttpServletRequest request){
+        String callback = request.getParameter("callback");
+        logger.info("callback",callback);
+        request.getSession().setAttribute("callback",callback);
         return new ModelAndView("index");
     }
 
-    @RequestMapping("/index2")
-    @ResponseBody
-    public Object index2(){
-        return "adadad";
+    @GetMapping("/auth/login")
+    public void login(HttpServletRequest request,HttpServletResponse response) throws IOException {
+        String callback = (String) request.getSession().getAttribute("callback");
+        logger.info("callback",callback);
+        Cookie cookie = new Cookie("token","xdfdfdfdfffffffffffffssdfsfsfsdfsdf");
+        cookie.setMaxAge(1000000);
+        cookie.setHttpOnly(true);
+        cookie.setPath("/");
+        cookie.setDomain("127.0.0.1");
+        response.addCookie(cookie);
+        response.sendRedirect(callback);
     }
 
-    @RequestMapping("/login")
-    public void login(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        String callback = request.getParameter("callback");
-        response.sendRedirect(callback);
+    @RequestMapping("/auth/info")
+    @ResponseBody
+    public Object info(HttpServletRequest request) {
+        Cookie[] cookies = request.getCookies();
+        if (null != cookies){
+            System.out.println(cookies);
+        }
+        User user = new User();
+        user.setName("xiao tian");
+        user.setUsername("m135");
+        user.setEmail("xiaotian@h3c,com");
+        return user;
     }
 }
---
+++ src/main/java/com/mock/login/controller/User.java
@@ -0,0 +1,32 @@
+package com.mock.login.controller;
+
+public class User {
+
+    private String username;
+    private String email;
+    private String name;
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+}
